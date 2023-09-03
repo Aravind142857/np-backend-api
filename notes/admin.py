ff0ac11485dcd96211217a44f1a947e6a94b5ea3
Aravind142857 <aravind.s2025@berkeley.edu>
2023-09-03 01:01:53 -0700

    Error Cors fixed

--- notes/admin.py
+++ notes/admin.py
@@ -4,11 +4,11 @@ from .models import Note, User
 class NoteModelAdmin(admin.ModelAdmin):
     list_display =('title', 'created_at', 'updated_at')
     search_fields=('title','description')
-    list_per_page=1
+    list_per_page=10
 class UserModelAdmin(admin.ModelAdmin):
     list_display =('email', 'note', 'created_at')
     search_fields=('email','note__title')
-    list_per_page=1
+    list_per_page=10
 admin.site.register(Note, NoteModelAdmin)
 admin.site.register(User, UserModelAdmin)
 # Register your models here.
