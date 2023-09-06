d15c2324c06a59e0861dbf9d912abaf192458140
Aravind142857 <aravind.s2025@berkeley.edu>
2023-09-06 13:29:08 -0700

    Create form

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import NoteListAPIView, NoteDetailAPIView, subscribeToNotesAPIView #,NoteSearchListAPIView
+from .views import NoteListAPIView, NoteDetailAPIView, subscribeToNotesAPIView, NoteCreateAPIView #,NoteSearchListAPIView
 urlpatterns=[
+    path('add', NoteCreateAPIView.as_view(), name="add"),
     path("notes/<str:slug>", NoteDetailAPIView.as_view(), name="note"),
     path('notes', NoteListAPIView.as_view(), name="notes"),
     path('subscribers', subscribeToNotesAPIView.as_view(), name="subscribe"),
+    
     # path("notes/search.<str:query>", NoteSearchListAPIView.as_view(), name="search")
 ]
--- notes/views.py
+++ notes/views.py
@@ -17,10 +17,29 @@ class NoteListAPIView(generics.ListAPIView):
 #             return response.Response(seld.serializer_class(query_set).data)
 #         return response.Reponse('Not found', status=status.HTTP_404_NOT_FOUND )
     
+class NoteCreateAPIView(generics.CreateAPIView):
+    serializer_class= NoteSerializer
+
+    
+    def get_queryset(self):
+        return Note.objects.all()
+    # fields = [
+    #     "title",
+    #     "description"
+    # ]
+    # def post(self, request):
+        
+    #     note = request.data.get('note')
+    #     serializer_class = NoteSerializer(data=note)
+    #     if serializer_class.is_valid(raise_exception=True):
+    #         note_saved = serializer_class.save()
+    #     return response.Response({"success":"Note '{}' saved successfully".format(note_saved.title)})
+
+        
 class NoteDetailAPIView(generics.ListAPIView):
     serializer_class= NoteSerializer
     def get(self, request, slug):
-        print("request")
+        
         query_set= Note.objects.filter(slug=slug).first()
     
         if query_set:
